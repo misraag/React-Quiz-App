32154a79402a19b0c53b7b271fb9055078612dbc
misraag <jaiofficial50@gmail.com>
2024-06-28 22:12:38 +0530

    added code for shuffled options and quiz end screen

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Options from "../Options/Options";
 import QUESTIONS from "../../questions.js";
+import quizCompleteImage from '../../assets/quiz-complete.png'
 import { AnswersContext } from "../../context/answers-context.jsx";
 
 export default function Quiz() {
   const [selectedAnswers, setSelectedAnswers] = useState([]);
 
-  const activeQuestion = selectedAnswers.length;
+  const activeQuestionIndex = selectedAnswers.length;
 
   function handleSelectingAnswer(answer) {
     setSelectedAnswers((prev) => [answer, ...prev]);
@@ -19,18 +20,30 @@ export default function Quiz() {
 
   function handleSelectAnswer(answer) {
     console.log(answer)
+    setSelectedAnswers((prevList) => [...prevList, answer])
   }
+
+  if(activeQuestionIndex === QUESTIONS.length) {
+    return <div id="summary">
+        <img src={quizCompleteImage}></img>
+        <h2>Quiz Completed!</h2>
+    </div>
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
 
   return (
     <div id="quiz">
       <div id="question">
         {/* <progress></progress> */}
-        <h2>{QUESTIONS[activeQuestion].text}</h2>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 
       
         <ul id="answers">
             {
-                QUESTIONS[activeQuestion].answers.map((answer) => {
+                shuffledAnswers.map((answer) => {
                     return <li key={answer} className="answer">
                         <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                     </li>
