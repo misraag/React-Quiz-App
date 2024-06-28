760cd209bfe05f67ef9e843cb9141c2cf82fe7fb
misraag <jaiofficial50@gmail.com>
2024-06-29 00:55:38 +0530

    corrected code for interval and timer changes for different questions

---
+++ src/components/QuestionProgress/Progress.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export default function Progress({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    console.log("Timer started");
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+        clearTimeout(timer)
+    }
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    console.log("Interval started");
+    const interval = setInterval(() => {
+      
+      setRemainingTime((prevTime) => prevTime - 100);
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <progress id="question-time" max={timeout} value={remainingTime}></progress>
+  );
+}
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,27 +1,13 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Options from "../Options/Options";
 import QUESTIONS from "../../questions.js";
 import quizCompleteImage from '../../assets/quiz-complete.png'
 import { AnswersContext } from "../../context/answers-context.jsx";
+import Progress from "../QuestionProgress/Progress.jsx";
 
 export default function Quiz() {
   const [selectedAnswers, setSelectedAnswers] = useState([]);
-
   const activeQuestionIndex = selectedAnswers.length;
-
-  function handleSelectingAnswer(answer) {
-    setSelectedAnswers((prev) => [answer, ...prev]);
-  }
-
-//   const answerContext = {
-//     answerList: selectedAnswers,
-//     onSelecting: handleSelectingAnswer,
-//   };
-
-  function handleSelectAnswer(answer) {
-    console.log(answer)
-    setSelectedAnswers((prevList) => [...prevList, answer])
-  }
 
   if(activeQuestionIndex === QUESTIONS.length) {
     return <div id="summary">
@@ -33,11 +19,30 @@ export default function Quiz() {
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
   shuffledAnswers.sort(() => Math.random() - 0.5);
 
+//   function handleSelectingAnswer(answer) {
+//     setSelectedAnswers((prev) => [answer, ...prev]);
+//   }
+
+//   const answerContext = {
+//     answerList: selectedAnswers,
+//     onSelecting: handleSelectingAnswer,
+//   };
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(answer) {
+    // console.log(answer)
+    setSelectedAnswers((prevList) => [...prevList, answer])
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
+
+
+
 
   return (
     <div id="quiz">
       <div id="question">
         {/* <progress></progress> */}
+        <Progress key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer}></Progress>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 
       
